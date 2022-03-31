f554d96c421263678eb5fcf30888cdd7c4ff5174
pengjiajun <t_t10010@163.com>
2022-03-31 18:02:51 +0800

    文本压缩0.2

--- compressText.cpp
+++ compressText.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
 #include <cstdio>
 #include <vector>
 #include <cstring>
@@ -34,32 +35,42 @@ struct HCANode {
 };
 
 
-//通过文本获取词频
-unordered_map<char, int> getFreq(string &text);
+
+//构建哈夫曼树
+HCTNode *buildHCodeTree(string &text);
 //通过词频构建哈夫曼树
 HCTNode *getHCodeTree(unordered_map<char, int> &freq);
 //通过树状存储哈夫曼树构建哈夫曼编码
 void getHCode(HCTNode *root, string u, unordered_map<char, string> &HCode);
-//通过顺序存储哈夫曼树构建哈夫曼编码
-void getHCode(vector<HCANode *> &arry, int idx, string u, unordered_map<char, string> &HCode);
 //通过哈夫曼编码转化文本
-string compressText(string &text, unordered_map<char, string> &HCode);
+string compressText(string &text, HCTNode *root);
+//获得压缩文本
+string getCompressText(string &text);
 //获得压缩文本的文件名
 string getCompFileName(string fileName);
 //获得解压缩文本的文件名
 string getDecompFileName(string fileName);
 //通过顺序哈夫曼树解压压缩的文本
-string getOrgText(string &compText, vector<HCANode *> HArry);
+string getOrgText(string &text);
 //将int按位分割为四位的string
 string num2str(int num);
 //将四位字符串转换为int
 int str2num(string s);
+//将哈夫曼树转换为字符串
+string HTree2str(HCTNode *root);
 //将哈夫曼树转换为数组形式
 void HTree2HArry(HCTNode *root, vector<HCANode *> &HArry);
 //将顺序哈夫曼树转换字符串
 string HArry2str(vector<HCANode *> &HArry);
 //将字符串还原为顺序哈夫曼树
 vector<HCANode *> str2HArry(string &s);
+
+//通过秘钥对文本解密
+bool encipher(string &text, string &passwd);
+//通过秘钥对文本解密
+bool dencipher(string &text, string &passwd);
+//通过字节数得到格式化的存储大小
+string textSize(int size);
 
 
 bool compress(string &fileName, string &passwd);
@@ -95,13 +106,133 @@ int main(int argc, const char *argv[]) {
 }
 
 
-//通过文本获取词频
-unordered_map<char, int> getFreq(string &text) {
+bool compress(string &fileName, string &passwd) {
+	ifstream fin;
+	ofstream fout;
+
+
+	//文件输入
+	fin.open(fileName, ifstream::binary);
+	if (!fin.is_open()) {
+		cout << "文件读取失败！" << endl;
+		return false;
+	}
+	fout.open(getCompFileName(fileName), ofstream::binary);
+
+
+	//读取文本
+	string text;
+	stringstream buffer;
+	buffer << fin.rdbuf();
+	text = buffer.str();
+
+	//获取压缩文本
+	string compText = getCompressText(text);
+
+	//加密压缩
+	if (passwd != "") {
+		if (encipher(compText, passwd)) {
+			cout << "Encipher success !" << endl;
+		} else {
+			cout << "Encipher failed !" << endl;
+		}
+	}
+
+	//将结果输出
+	fout.write(compText.c_str(), compText.length());
+
+	//压缩率计算
+	int orgSize = text.size();
+	int cmpSize = compText.size();
+	cout << "Original text size:\t" << textSize(orgSize) << endl;
+	cout << "Compressed text size:\t" << textSize(cmpSize) << endl;
+	cout << "Compression rate:\t" << (cmpSize > orgSize ? '+' : ' ');
+	cout << setprecision(4) << (double)( cmpSize - orgSize) / orgSize * 100 << '\%' << endl << endl;
+
+	//数据流关闭
+	fin.close();
+	fout.close();
+	return orgSize > cmpSize;
+}
+
+bool decompress(string &fileName, string &passwd) {
+	ifstream fin;
+	ofstream fout;
+
+	//文件输入
+	fin.open(fileName, ifstream::binary);
+	if (!fin.is_open()) {
+		cout << "file read error !" << endl;
+		return false;
+	}
+	fout.open(getDecompFileName(fileName), ofstream::binary);
+
+
+	//读取压缩文本
+	string text;
+	stringstream buffer;
+	buffer << fin.rdbuf();
+	text = buffer.str();
+
+	//解密
+	if (passwd != "") {
+		dencipher(text, passwd);
+	}
+
+	string orgText = getOrgText(text);
+
+
+	//结果输出
+	fout.write(orgText.c_str(), orgText.length());
+
+
+	//解压结果
+	int orgSize = orgText.size();
+	int cmpSize = text.size();
+	cout << "Compressed text size:\t" << textSize(cmpSize) << endl;
+	cout << "Original text size:\t" << textSize(orgSize) << endl << endl;
+
+
+	//数据流关闭
+	fin.close();
+	fout.close();
+	return true;
+}
+
+void help() {
+	cout << "* -h\t\t\tGet help" << endl << endl;
+	cout << "* -zip filename\t\tCompress" << endl;
+	cout << "\t\t\t-filename" << endl;
+	cout << "\t\t\t-passwd" << endl << endl;
+	cout << "* -unzip filename\tDecompress" << endl;
+	cout << "\t\t\t-filename" << endl;
+	cout << "\t\t\t-passwd" << endl << endl;
+}
+
+
+//获得压缩文本
+string getCompressText(string &text) {
+
+	//根据文本构建哈夫曼树
+	HCTNode *root = buildHCodeTree(text);
+
+
+	//顺序化哈夫曼树
+	string HArrystr = HTree2str(root);
+
+
+	//根据哈夫曼树压缩文本
+	return HArrystr + compressText(text, root);
+}
+
+//构建哈夫曼树
+HCTNode *buildHCodeTree(string &text) {
 	unordered_map<char, int> freq;
 	for (char ch : text) {
 		freq[ch]++;
 	}
-	return freq;
+
+	return getHCodeTree(freq);
 }
 
 
@@ -137,21 +268,13 @@ void getHCode(HCTNode *root, string u, unordered_map<char, string> &HCode) {
 }
 
 
-//通过顺序存储哈夫曼树构建哈夫曼编码
-void getHCode(vector<HCANode *> &arry, int idx, string u, unordered_map<char, string> &HCode) {
-	if (idx == -1)
-		return;
-	if (arry[idx]->ch) {
-		HCode[arry[idx]->ch] = u;
-	}
-
-	getHCode(arry, arry[idx]->left, u + "1", HCode);
-	getHCode(arry, arry[idx]->right, u + "0", HCode);
-}
-
-
 //通过哈夫曼编码转化文本
-string compressText(string &text, unordered_map<char, string> &HCode) {
+string compressText(string &text, HCTNode *root) {
+
+	//构建编码表
+	unordered_map<char, string> HCode;
+	getHCode(root, "", HCode);
+
 	string comp;
 	int idx = 0;
 	char comp_t = 0;
@@ -204,18 +327,29 @@ string getDecompFileName(string fileName) {
 
 
 //通过顺序哈夫曼树解压压缩的文本
-string getOrgText(string &compText, vector<HCANode *> HArry) {
+string getOrgText(string &text) {
+
+	//将压缩文件各组件分割出来
+	int len_HArry = str2num(text.substr(0, 4));
+	string HArrystr = text.substr(4, len_HArry * 9);
+	string compText = text.substr(4 + len_HArry * 9);
+
+
+	//通过压缩文件中的信息恢复文本
+	vector<HCANode *> orgHArry = str2HArry(HArrystr);
+
+	//还原原文本内容
 	string orgText;
 	int redun = compText.back() + 8;
 	long long len = (long long)(compText.length()) * 8 - redun;
 
-	HCANode *tmp = HArry[0];
+	HCANode *tmp = orgHArry[0];
 	for (long long idx = 0; idx < len; idx++) {
-		tmp = ((compText[idx / 8] & (1 << (idx % 8))) ? HArry[tmp->left] : HArry[tmp->right]);
+		tmp = ((compText[idx / 8] & (1 << (idx % 8))) ? orgHArry[tmp->left] : orgHArry[tmp->right]);
 
 		if (tmp->ch) {
 			orgText.push_back(tmp->ch);
-			tmp = HArry[0];
+			tmp = orgHArry[0];
 		}
 	}
 
@@ -241,6 +375,13 @@ int str2num(string s) {
 	return ans;
 }
 
+
+//将哈夫曼树转换为字符串
+string HTree2str(HCTNode *root) {
+	vector<HCANode *>  HArry;
+	HTree2HArry(root, HArry);
+	return HArry2str(HArry);
+}
 
 //将哈夫曼树转换为数组形式
 void HTree2HArry(HCTNode *root, vector<HCANode *> &HArry) {
@@ -279,6 +420,7 @@ vector<HCANode *> str2HArry(string &s) {
 	return ans;
 }
 
+//通过秘钥对文本加密
 bool encipher(string &text, string &passwd) {
 	int len1 = text.size(), len2 = passwd.size();
 	if (len2 > len1)
@@ -291,121 +433,26 @@ bool encipher(string &text, string &passwd) {
 	return true;
 }
 
+
+//通过秘钥对文本解密
 bool dencipher(string &text, string &passwd) {
 	return encipher(text, passwd);
 }
 
-bool compress(string &fileName, string &passwd) {
-	ifstream fin;
-	ofstream fout;
 
+//通过字节数得到格式化的存储大小
+string textSize(int size) {
+	double ans = size;
 
-	//文件输入
-	fin.open(fileName, ifstream::binary);
-	if (!fin.is_open()) {
-		cout << "文件读取失败！" << endl;
-		return false;
-	}
-	fout.open(getCompFileName(fileName), ofstream::binary);
-
-
-	//读取文本
-	string text;
-	stringstream buffer;
-	buffer << fin.rdbuf();
-	text = buffer.str();
-
-
-	//根据词频创建编码表
-	unordered_map<char, int> freq = getFreq(text);
-	HCTNode *root = getHCodeTree(freq);
-	unordered_map<char, string> HCode;
-	getHCode(root, "", HCode);
-
-
-	//将顺序化哈夫曼树
-	vector<HCANode *>  HArry;
-	HTree2HArry(root, HArry);
-	string HArrystr = HArry2str(HArry);
-
-
-	//根据编码表压缩文本并存入哈夫曼树作为压缩结果
-	string compText = HArrystr + compressText(text, HCode);
-
-
-	//加密压缩
-	if (passwd != "") {
-		if (encipher(compText, passwd)) {
-			cout << "Encipher success !" << endl;
-		} else {
-			cout << "Encipher failed !" << endl;
-		}
+	const string CompSize[4] = {"B", "KB", "MB", "GB"};
+	int idx = 0;
+	while (ans > 1024) {
+		ans /= 1024;
+		idx++;
 	}
 
-	//将结果输出
-	fout.write(compText.c_str(), compText.length());
+	stringstream ss;
+	ss << setprecision(5) << ans;
 
-
-	//数据流关闭
-	fin.close();
-	fout.close();
-	return true;
-}
-
-bool decompress(string &fileName, string &passwd) {
-	ifstream fin;
-	ofstream fout;
-
-	//文件输入
-	fin.open(fileName, ifstream::binary);
-	if (!fin.is_open()) {
-		cout << "file read error !" << endl;
-		return false;
-	}
-	fout.open(getDecompFileName(fileName), ofstream::binary);
-
-
-	//读取压缩文本
-	string text;
-	stringstream buffer;
-	buffer << fin.rdbuf();
-	text = buffer.str();
-
-	//解密
-	if (passwd != "") {
-		dencipher(text, passwd);
-	}
-
-	//将压缩文件各组件分割出来
-	int len = str2num(text.substr(0, 4));
-	string HArrystr = text.substr(4, len * 9);
-	string compText = text.substr(4 + len * 9);
-
-
-	//通过压缩文件中的信息恢复文本
-	vector<HCANode *> orgHArry = str2HArry(HArrystr);
-	string orgText = getOrgText(compText, orgHArry);
-
-//	for (auto &t : orgHArry) {
-//		cout << t->ch << " " << t->left << " " << t->right << endl;
-//	}
-
-	//结果输出
-	fout.write(orgText.c_str(), orgText.length());
-
-
-	//数据流关闭
-	fin.close();
-	fout.close();
-	return true;
-}
-
-void help() {
-	cout << "* -h\t\t\tGet help" << endl << endl;
-	cout << "* -zip filename\t\tCompress" << endl;
-	cout << "\t\t\t-filename" << endl;
-	cout << "\t\t\t-passwd" << endl << endl;
-	cout << "* -unzip filename\tDecompress" << endl;
-	cout << "\t\t\t-filename" << endl;
-	cout << "\t\t\t-passwd" << endl << endl;
+	return ss.str() + CompSize[idx];
 }
